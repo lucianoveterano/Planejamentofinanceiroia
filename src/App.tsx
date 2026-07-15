f6f6cf093ffb82e19e74c2df1f5f40f528ff974c
luciano-vtn <sgtlucianobm@gmail.com>
2026-07-15 12:35:13 -0300

    Configurando rotas,  configuraçao de temas(claro e escuro)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './router'
+
 function App() {
-  return (
-    <>
-      <h1>Inicio do projeto</h1>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { fileURLToPath } from 'node:url'
 
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
